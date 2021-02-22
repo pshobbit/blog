fce182f34fec814eb9432d94a3b14d72abc002ec
Pedro Suarez <pedrosuarezmula@gmail.com>
2021-02-22 21:26:03 +0100

    BLog

--- blog/settings.py
+++ blog/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogs.apps.BlogsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogs.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
